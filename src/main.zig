0d7f81f47ae122d914eededc17599695c8090fad
OmarBouchoucha0 <bouchouchaomar38@gmail.com>
2026-06-24 15:11:46 +0100

    stderr and file openeing (not tested)

--- src/main.zig
+++ src/main.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const Io = std.Io;
 const stdout = std.Io.File.stdout();
 const stdin = std.Io.File.stdin();
+const stderr = std.Io.File.stderr();
 
 const interpeter = @import("interpeter");
 const print = std.debug.print;
@@ -9,13 +10,16 @@ const print = std.debug.print;
 pub fn main(init: std.process.Init) !void {
     const args_iter = init.minimal.args;
     const args = try args_iter.toSlice(init.arena.allocator());
+    const io = init.io;
+
     switch (args.len) {
         1 => {
             print("executing directly in the shell\n", .{});
-            try runPrompt(init);
+            try runPrompt(io);
         },
         2 => {
             print("executing file\n", .{});
+            try runFile(io, args[1]);
         },
         else => {
             print("non supported number of args\n", .{});
@@ -24,8 +28,7 @@ pub fn main(init: std.process.Init) !void {
     }
 }
 
-pub fn runPrompt(init: std.process.Init) !void {
-    const io = init.io;
+pub fn runPrompt(io: std.Io) !void {
     while (true) {
         const gpa = std.heap.ArenaAllocator;
         var arena = gpa.init(std.heap.page_allocator);
@@ -47,7 +50,38 @@ pub fn runPrompt(init: std.process.Init) !void {
             try writer.interface.flush();
             break;
         }
-        try writer.interface.print("PLACEHOLDER : {s}\n", .{read_buffer[0..n]});
-        try writer.interface.flush();
+        @panic("call the compiler here");
     }
+}
+
+pub fn runFile(io: std.Io, file_name: []const u8) !void {
+    const gpa = std.heap.ArenaAllocator;
+    var arena = gpa.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    var file = std.Io.Dir.cwd().openFile(io, file_name, .{}) catch {
+        try printErr(io, allocator, "Could not find File");
+        return error.FileNotFound;
+    };
+    defer file.close(io);
+    @panic("call the compiler hre");
+}
+
+pub fn printErr(io: std.Io, allocator: std.mem.Allocator, msg: []const u8) !void {
+    const write_buffer = try allocator.alloc(u8, 1024);
+    var writer = std.Io.File.writer(stderr, io, write_buffer);
+    try writer.interface.print("[ERR] : {s}\n", .{msg});
+    try writer.interface.flush();
+}
+
+pub fn printCompileErr(io: std.Io, allocator: std.mem.Allocator, line: u32, col: u32, msg: []const u8) !void {
+    const write_buffer = try allocator.alloc(u8, 1024);
+    var writer = std.Io.File.writer(stderr, io, write_buffer);
+    try writer.interface.print("[ERR] at {d}:{d} -> {s}\n", .{ line, col, msg });
+    try writer.interface.flush();
+}
+
+pub fn compile() !void {
+    @panic("todo!");
 }
